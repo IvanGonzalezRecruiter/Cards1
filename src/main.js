581e744578c5dd2ddca3ec822ab7f21859c9d735
IvanGonzalezRecruiter <ivan.gonzalez25@gmail.com>
2022-05-05 01:13:21 +0000

    .

--- src/main.js
+++ src/main.js
@@ -5,11 +5,10 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let suits = ["heart", "diamond", "spade", "club"];
+  let palos = ["heart", "diamond", "spade", "club"];
 
   let value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
-  // Always returns a random number between min and max (both included)
   function getRndInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
   }
@@ -17,19 +16,18 @@ window.onload = function() {
   let card = document.querySelector(".card");
   let cardValue = document.querySelector("h1");
 
-  // classList accesses css classes and reflects it on DOM via add
   cardValue.innerHTML = value[getRndInt(0, value.length)];
 
-  card.classList.add(suits[getRndInt(0, suits.length)]);
+  card.classList.add(palos[getRndInt(0, palos.length)]);
 
-  document.querySelector(".btn").textContent = "Deal";
+  document.querySelector(".btn").textContent = "Repartir";
 
   document.querySelector(".btn").addEventListener("click", function() {
-    for (let i = 0; i < suits.length; i++) {
-      if (card.classList.contains(suits[i])) {
-        card.classList.remove(suits[i]);
+    for (let i = 0; i < palos.length; i++) {
+      if (card.classList.contains(palos[i])) {
+        card.classList.remove(palos[i]);
 
-        card.classList.add(suits[getRndInt(0, suits.length)]);
+        card.classList.add(palos[getRndInt(0, palos.length)]);
       }
     }
 
