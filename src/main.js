cb64c14868cc8b0f2cfec52ff203ba5ddad5f153
IvanGonzalezRecruiter <ivan.gonzalez25@gmail.com>
2022-05-10 03:03:16 +0000

    .

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let palos = ["heart", "diamond", "spade", "club"];
+  let palos = ["corazon", "diamante", "pica", "trebol"];
 
   let value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "AS"];
 
